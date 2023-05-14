ec963821311e198d9b629b5a043a6e9472464a05
dreipunkteinsvier <dreipunkteinsvier@gmail.com>
2023-05-14 22:19:25 +0200

    initial commit

---
+++ config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 dreipunkteinsvier (@dreipunkteinsvier)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#ifdef RGB_MATRIX_ENABLE
+    #define RGB_MATRIX_LED_COUNT RGBLED_NUM
+    #define DRIVER_LED_TOTAL RGBLED_NUM
+    #define WS2812_DI_PIN GP25
+#endif
---
+++ keymaps/vial/rules.mk
@@ -0,0 +1,8 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+ENCODER_MAP_ENABLE = yes
+
+VIALRGB_ENABLE = yes
+
+COMBO_ENABLE = yes
